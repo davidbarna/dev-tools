6502ba1866304a2e3b31f4a32ebbf7e79a2a0e1b
David Almeida <dalmeida@atlassian.com>
2019-04-27 11:33:46 +1000

    babel-preset

---
+++ packages/babel-preset/babel.config.js
@@ -0,0 +1,16 @@
+module.exports = api => {
+  api.assertVersion(7);
+
+  return {
+    presets: [
+      [require("@babel/preset-env"), {}],
+      require("@babel/preset-typescript"),
+      require("@babel/preset-react")
+    ],
+    plugins: [
+      require("@babel/plugin-proposal-class-properties"),
+      require("@babel/plugin-syntax-dynamic-import"),
+      require("@babel/plugin-transform-runtime")
+    ]
+  };
+};
