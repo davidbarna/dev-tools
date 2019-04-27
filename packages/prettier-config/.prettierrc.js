131c3b2c0fb1c2902da90b397511dc321b4a6de4
David Almeida <dalmeida@atlassian.com>
2019-04-27 12:58:46 +1000

    prettier-config package

---
+++ packages/prettier-config/.prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  // semi: true, // true
+  // printWidth: 80, // 80
+  // tabWidth: 2, // 2
+  // useTabs: false, // false
+  singleQuote: true, // false
+  jsxSingleQuote: true, // false
+  quoteProps: 'as-needed', // as-needed|consistent|preserve
+  trailingComma: 'all', // none|es5|all
+  // bracketSpacing: true, // true
+  // jsxBracketSameLine: false, //false
+  arrowParens: 'always', // avoid|always
+  proseWrap: 'preserve', // preserve|always|never|
+};
